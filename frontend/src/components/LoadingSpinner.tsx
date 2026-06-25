5e29f1a0b6db20685a62b02318d4700b490147d9
hizkia sirait <hizkiasirait12@gmail.com>
2026-06-25 16:07:54 +0700

    delete

--- frontend/src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center py-20">
-      <div className="w-10 h-10 border-2 border-white/10 border-t-gold-400 rounded-full animate-spin" />
-    </div>
-  );
-}
--- frontend/src/services/index.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from './AuthService';
-import { FormService } from './FormService';
-import { DashboardService } from './DashboardService';
-import { UserService } from './UserService';
-
-export const authService = new AuthService();
-export const formService = new FormService();
-export const dashboardService = new DashboardService();
-export const userService = new UserService();
